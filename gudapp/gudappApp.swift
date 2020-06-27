45a5dca4b61f074fcc2ebd52d6f52bdd81ab8a5c
Brian Law <brian.law@icloud.com>
2020-06-26 20:14:19 -0700

    Initial Commit

---
+++ gudapp/gudappApp.swift
@@ -0,0 +1,17 @@
+//
+//  gudappApp.swift
+//  gudapp
+//
+//  Created by Brian Law on 6/26/20.
+//
+
+import SwiftUI
+
+@main
+struct gudappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
